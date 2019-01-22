e300c952f838efe5824457c650c161acac187c00
mpabarca <mpabarca@uc.cl>
2019-01-22 10:16:51 -0300

    main.js se agrega esqueleto del DOM

---
+++ main.js
@@ -0,0 +1,22 @@
+//MANEJO DE DOM
+
+/*  FETCH
+let datajson;
+fetch("llamada-data")
+ .then(data=>data.json())
+ .then(data=>{
+   datajson=Object.values(data.data);
+})
+*/
+
+/* SE LLAMA A BOOTSTRAP
+document.addEventListener('DOMContentLoaded', function(){
+  window.M.AutoInit(); 
+});
+*/
+
+/* INICIO DE TODO EL DOM
+window.onload =() =>{
+
+};
+*/

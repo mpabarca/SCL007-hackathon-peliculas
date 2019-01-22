3c4327c1ba7955211d1a7884c05ea69f6ba04009
mpabarca <mpabarca@uc.cl>
2019-01-22 10:47:17 -0300

    se crean nuevas carpetas + creacion archivos TDD

---
+++ movies.js
@@ -0,0 +1,16 @@
+//AQUI SE AGREGAN TODAS LAS FUNCIONES A USAR EN DOM
+
+/* ESTRUCTURA BASE DE LA FUNCION
+
+window.functions ={
+    nombreFuncion: (parametros) => {
+
+        return resultadoFuncion;
+    },
+    nombreFuncion2: (parametros) => {
+
+        return resultadoFuncion2;
+    }
+}
+
+*/

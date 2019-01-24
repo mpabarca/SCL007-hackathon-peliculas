ea3f8cf7a1ddba022a779a68564043d31366903f
mpabarca <mpabarca@uc.cl>
2019-01-23 23:29:12 -0300

    movie.js se mueven todas las funciones a main.js

--- src/main.js
+++ src/main.js
@@ -13,7 +13,9 @@ fetch("llamada-data")
 // INICIO DE TODO EL DOM
 window.onload =() =>{
 
+//FUNCIONES
 
+/* Función que filtra toda la data respecto a un nombre y las muestra */
 function getMoviesByName(searchText){
   axios.get('http://www.omdbapi.com/?apikey=8f262e4a&s='+searchText)
   .then((response)=>{
@@ -38,6 +40,55 @@ function getMoviesByName(searchText){
   }); 
 } 
 
+/* Función que filtra toda la data respecto a una película en especial está lista para generar el modal */
+function getMovieById(movieId){
+  axios.get('http://www.omdbapi.com/?apikey=8f262e4a&i='+movieId+'&plot=full')
+  .then((response)=>{
+      console.log(response);
+      let movie = response.data;
+      let moviesFound = ` 
+      <div id="modal1" class="modal">
+          <div class="modal-content">
+              <div class = "col-md-4">
+                  <img src = "${movie.Poster}" class="thumbnail">
+              </div>
+          <h4>${movie.Title}</h4>
+          <ul class = "list-group">
+                  <li class = "list-group-item"><strong>Género:</strong>${movie.Genre} </li>
+                  <li class = "list-group-item"><strong>Género:</strong>${movie.Genre} </li>
+                  <li class = "list-group-item"><strong>Género:</strong>${movie.Genre} </li>
+                  <li class = "list-group-item"><strong>Género:</strong>${movie.Genre} </li>
+                  <li class = "list-group-item"><strong>Género:</strong>${movie.Genre} </li>
+                  <li class = "list-group-item"><strong>Género:</strong>${movie.Genre} </li>
+                  <li class = "list-group-item"><strong>Género:</strong>${movie.Genre} </li>
+              </ul>
+          </div>
+          <div class="modal-footer">
+          <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+          </div>
+      </div>
+
+      ` ;
+  })
+  .catch((err) => {
+    console.log(err);
+  }); 
+}
+
+/* Función que filtra toda la data respecto a una película en especial y muestra el Rating */
+function getRatingByName(movieName){
+  axios.get('http://www.omdbapi.com/?apikey=8f262e4a&t='+movieName+'&plot=full')
+  .then((response) => {
+      console.log('nombre ' + movieName)
+      console.log(response.data.imdbRating)
+      $('#rank').html(response.data.imdbRating);
+  })
+  .catch((err) => {
+      console.log(err);
+  }); 
+}
+
+//BUSQUEDA POR NOMBRE
 let moviesFound = document.getElementById('show-movies');
 
 document.getElementById('search-form').addEventListener('submit',(event) => {
@@ -46,6 +97,6 @@ document.getElementById('search-form').addEventListener('submit',(event) => {
     getMoviesByName(searchText);
     moviesFound = '';    
 });
-
+//BUSQUEDA POR GENERO
 
 };
